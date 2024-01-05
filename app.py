7173e52ba9a7bc4c9513a7cd9e640188755d445f
0xreddy <0xreddy@gmail.com>
2024-01-06 00:58:08 +0530

    Update AWS ECS configuration and modify hello_world message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'HELLO_WORLD from Flask.'
+    return 'HELLO_WORLD from Flask.!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
